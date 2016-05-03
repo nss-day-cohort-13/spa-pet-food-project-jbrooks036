1b94c8e276e030951ae6f56467a874b335c0a829
jbrooks036 <joannbrooks036@gmail.com>
2016-05-03 12:51:16 -0500

    first start on building display table

--- display-food.js
+++ display-food.js
@@ -1,13 +1,25 @@
 function showDogFoods (dogFoods) {
-  console.log("display-food.js-2 / dogFoods = ", dogFoods);
-  console.log("display-food.js-3 / dogFoods.dog_brands[0] = ", dogFoods.dog_brands[0]);
+  // console.log("display-food.js-2 / dogFoods = ", dogFoods);
+  // console.log("display-food.js-3 / dogFoods.dog_brands[0] = ", dogFoods.dog_brands[0]);
 
   var dogFoodBrand = document.getElementById("brand-name");
   var brandname = dogFoods.dog_brands[0].name;
-  console.log("display-food.js-7 / brandname = ", brandname);
+  // console.log("display-food.js-7 / brandname = ", brandname);
 
   dogFoodBrand.innerHTML = brandname;
+  var numBrands = dogFoods.dog_brands.length;
+  console.log("display-food.js-11 / numBrands = ", numBrands);
 
+  var dogfoodTable = document.getElementById("dog-food-table");
+  console.log("display-food.js-14 / dogfoodTable = ", dogfoodTable);
+  for (i=0; i < numBrands; i++) {
+
+    dogfoodTable.innerHTML += "<tr>"
+      + "<td>"
+      + dogFoods.dog_brands[i].name
+      + "</td>"
+      + "</tr>";
+  }
 }
 
 food.loadDogFoods(showDogFoods);
